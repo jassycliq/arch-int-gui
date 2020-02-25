d1aae4aef8994fa59e9d1839876c1a835403cc8f
Jose Salgado <josalgado12709@gmail.com>
2020-02-24 19:23:04 -0800

    Update install.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -22,7 +22,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
 ####
 
 # define pacman packages
-pacman_packages="ttf-dejavu xorg-fonts-misc terminus-font ttf-dejavu xfce4-terminal tint2 xorg-server-xvfb tigervnc openbox obconf python2-xdg coreutils lxappearance xcompmgr cantarell-fonts python-pip python-numpy"
+pacman_packages="ttf-dejavu xorg-fonts-misc terminus-font ttf-dejavu xfce4-terminal tint2 xorg-server nvidia tigervnc openbox obconf python2-xdg coreutils lxappearance xcompmgr cantarell-fonts python-pip python-numpy"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
